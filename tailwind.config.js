f11bbad7a29d89b55b0eea006ea6ba6a275a7e2d
han <jaehun_@kakao.com>
2025-03-21 17:58:18 +0900

    css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}", // Next.js app directory 지원
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
