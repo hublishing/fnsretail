be5cfccec6389d4f8c2c435e49e48be03ac016fe
han <jaehun_@kakao.com>
2025-03-24 12:26:38 +0900

    bigquery

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import SidebarWrapper from "./components/sidebar-wrapper";
+import { SidebarWrapper } from "./components/sidebar-wrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
